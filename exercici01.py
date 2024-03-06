e11d940d295bc6503e4968a6d0d4707683988081
n4nd1k4nd3 <74754768+n4nd1k4nd3@users.noreply.github.com>
2024-03-06 21:41:47 +0100

    primer exercici fet

---
+++ exercici01.py
@@ -0,0 +1,17 @@
+
+
+# llista de llistes
+
+trimestres2 = [
+    ["gener", "febrer", "març"], 
+    ["abril", "maig", "juny"], 
+    ["juliol", "agost", "setembre"], 
+    ["octubre", "novembre", "decembre"]
+]
+
+
+print(trimestres2)
+
+
+
+
