33a58c7d48057275c89c29ed9e76d4fff4cd924f
n4nd1 <nand1kande@gmail.com>
2024-03-10 22:02:45 +0100

    exe2_i_3_per_acabar

--- Tasca_M02_T02.py
+++ Tasca_M02_T02.py
@@ -5,8 +5,6 @@ L'exercici consisteix a crear una funció que et classifiqui una variable numèr
 Recorda que Suspès < 5, Aprovat > 5 i < 7, Notable > 7 i < 9 i Excel·lent > 9.
 '''
 
-# Exercici 1 funcio
-
 def notes(nota):
     if nota < 5: 
         return "Suspès"
@@ -17,5 +15,38 @@ def notes(nota):
     else: 
         return "Excelent"
 
-primera_nota = notes(7)
-print(primera_nota)
+resultat_1 = notes(7)
+print(resultat_1)
+
+''' Exercici 2
+Utilitzant el següent tutorial crea una funció que et pregunti dos números. T’ha de mostrar un missatge dient si el primer és més gran, el segon és més gran o són iguals.'''
+
+'''var1 = input("Introdueix el primer número a comparar:\n")
+var2 = input("Introdueix el segon número a comparar:\n")
+             
+def comparar_numeros(num1: int, num2: int):
+    if num1 > num2:
+        return f"El primer numero, el {num1} es mes gran que el segon número el {num2}"
+    elif num1 < num2:
+        return f"El segon numero, el {num2} es mes gran que el primer número el {num1}"
+    else:
+        return f"El nombres son iguals"
+
+resultat_2 = comparar_numeros(var1, var2)
+print(resultat_2)'''
+
+'''- Exercici 3
+Crea una funció que et pregunti el teu nom, i et demani un número. Si el número és 0, hauria de mostrar un missatge d’error. En cas contrari, hauria de mostrar el nom repetit tants cops com indiqui el número. Per exemple, “Joan Joan Joan”.
+'''
+
+nom = input("Introdueix en nom:\n")
+num_de_noms = int(input("Introdueix el número de vegades que vols que es repeteixi el nom:\n"))
+
+def noms_repetits(nom: str, num_de_noms: int):
+    if num_de_noms == 0:
+        return "Error, has indicat 0 vegades"
+    else:
+        return f"{nom * num_de_noms}"
+    
+resultat3 = noms_repetits(nom, num_de_noms)
+print(resultat3)
