1b1136a474a5d3a098ca18b7d5a9a56bb095997d
n4nd1 <nand1kande@gmail.com>
2024-03-11 12:24:42 +0100

    acabat_ex4

--- Tasca_M02_T02.py
+++ Tasca_M02_T02.py
@@ -39,7 +39,7 @@ print(resultat_2)'''
 Crea una funció que et pregunti el teu nom, i et demani un número. Si el número és 0, hauria de mostrar un missatge d’error. En cas contrari, hauria de mostrar el nom repetit tants cops com indiqui el número. Per exemple, “Joan Joan Joan”.
 '''
 
-nom = input("Introdueix en nom:\n")
+'''nom = input("Introdueix en nom:\n")
 num_de_noms = int(input("Introdueix el número de vegades que vols que es repeteixi el nom:\n"))
 
 def noms_repetits(nom: str, num_de_noms: int):
@@ -49,4 +49,35 @@ def noms_repetits(nom: str, num_de_noms: int):
         return f"{nom} " * num_de_noms
     
 resultat3 = noms_repetits(nom, num_de_noms)
-print(resultat3)
+print(resultat3)'''
+
+'''- Exercici 4
+Crea una funció que donada una llista qualsevol, et digui si és simètrica o no. Si ho és, que et digui quants elements té.'''
+
+def es_simetrica(llista):
+    llargada = len(llista) 
+    meitat = llargada // 2 
+
+    if llargada < 2:
+        return "La llista es simètrica i té 1 element." if llargada == 1 else "La llista està buida."
+    
+    for i in range(meitat):
+        if llista[i] != llista[llargada - i - 1]:  
+            return "La llista no és simètrica."
+    
+ 
+    return f"La llista es simètrica i té {llargada} elements."
+
+
+llista_1= [6, 7, 8, 9, 10, 9, 8, 7, 6]
+llista_2 = [6, 7 ,8, 9, 10, 11, 12, 13, 14]
+
+resultat_41 = es_simetrica(llista_1)
+resultat_42 = es_simetrica(llista_2)
+
+print(resultat_41)
+print(resultat_42)
+
+'''- Exercici 5
+Crea una funció que donada una llista, et digui quants números coincideixen amb la seva posició. Per exemple [3,4,2,0,2,3,6] el 2 i el 6 coincideixen.
+'''
